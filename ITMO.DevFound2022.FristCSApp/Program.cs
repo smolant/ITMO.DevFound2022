e4b3e50e34dcf8fe7a162945c21fe5e38cfb08fd
Anton Smolianinov <smolant@hotmail.com>
2022-04-07 21:41:45 +0300

    Add project files.

---
+++ ITMO.DevFound2022.FristCSApp/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITMO.DevFound2022.FristCSApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string repeat = "no";
+            do
+            {
+                ToDo();
+
+                Console.WriteLine("Продолжать?");
+                repeat = Console.ReadLine();
+            
+            }
+            while (repeat != "no");
+
+            Console.WriteLine("Пока!");
+        }
+
+        static void ToDo()
+        {
+            Console.WriteLine("Введи строку: ");
+            string result = Console.ReadLine();
+
+            Console.WriteLine(result == "ivan");
+
+            //if (result == "ivan")
+            //{
+            //    Console.WriteLine("true");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("false");
+            //}
+
+            Console.WriteLine("имя = " + result);
+        }
+    }
+}
